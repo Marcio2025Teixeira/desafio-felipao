a91aaef9e62ca4bd89713f4947908dbdd80d1bc8
Marcio2025Teixeira <mntex2013@gmail.com>
2026-02-16 22:20:40 -0300

    Projeto de desafio Felipão realizado

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("oi")
+// Desafio Classificador de nível de Herói
+
+// Variáveis
+let nome = "Marcio NTeix";
+let xp = 9898;
+let nivel = "";
+
+// Estrutura de decisão
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp <= 5000) {
+    nivel = "Prata";
+} else if (xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp <= 8000) {
+    nivel = "Platina";
+} else if (xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
